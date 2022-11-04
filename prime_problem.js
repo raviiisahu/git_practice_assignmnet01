6ad7488070a8772574b8745a204ecb2012c6d127
unknown <rvsahu8350@gmail.com>
2022-11-04 23:22:02 +0530

    new code

--- prime_problem.js
+++ prime_problem.js
@@ -1,13 +1,13 @@
-let num = 91;
+let number = 91;
 let count=0;
-for(let i=1; i<num; i++){
-    if(num%i==2){
+for(let i=1; i<=number; i++){
+    if( num%i == 0 ){
         count++;
     }
 }
 if(count==2){
-    console.log("Yes")
+    console.log("It's a prime number.");
 }
 else{
-    console.log("No")
+    console.log("It's not a prime number.");
 }
